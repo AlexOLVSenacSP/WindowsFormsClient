c7d6834cd1bc879e20fc23f06a25a81564b6311a
ALEXANDRE DE OLIVEIRA <alexandre.oliveira15@senacsp.edu.br>
2025-09-23 10:38:38 -0300

    Home(Panel Blue)

--- WindowsFormsClient/FormHome.Designer.cs
+++ WindowsFormsClient/FormHome.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlAzul = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlAzul
+            // 
+            this.pnlAzul.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pnlAzul.Location = new System.Drawing.Point(0, 0);
+            this.pnlAzul.Name = "pnlAzul";
+            this.pnlAzul.Size = new System.Drawing.Size(400, 700);
+            this.pnlAzul.TabIndex = 9;
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 700);
+            this.Controls.Add(this.pnlAzul);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormHome";
             this.Text = "FormHome";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel pnlAzul;
     }
 }
--- WindowsFormsClient/FormHome.cs
+++ WindowsFormsClient/FormHome.cs
@@ -16,5 +16,7 @@ namespace WindowsFormsClient
         {
             InitializeComponent();
         }
+
+        
     }
 }
