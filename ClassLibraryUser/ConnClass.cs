47102b5ddcc8a763f847ddc1589a90155368e42b
ALEXANDRE DE OLIVEIRA <alexandre.oliveira15@senacsp.edu.br>
2025-10-07 11:47:07 -0300

    Conexao Banco de dados Classe de conexao.

---
+++ ClassLibraryUser/ConnClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ClassLibraryUser
+{
+    public class ConnClass
+    {
+        //Propriedades
+        private const string Server = "TAU0712134W11-1";
+        private const string Database  = "DB_Library";
+        private const string User = "senac";
+        private const string Password = "senac";
+
+        //string de conexão
+        protected string StrConn = $"Server={Server}; Database={Database}; User Id={User}; Password={Password};";
+
+        //Metodo para conectar com Banco de Dados.
+        public SqlConnection Connection ()
+        {
+            SqlConnection _conn = new SqlConnection(StrConn);
+            return _conn;
+        }
+
+
+    }
+}
